f17594e0ecf30724aaf5d3ae2dd4fef61290d292
Tashkyn Murat <tashkynmurat@gmail.com>
2021-08-06 16:18:14 +0600

    implement API test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [meanings, setMeanings] = useState([]);
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
   return <div className="App">Hello World</div>;
 }
 
