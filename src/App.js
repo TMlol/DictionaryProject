8541c964e77fea1ed3638dac987afe89c5dbef11
Tashkyn Murat <tashkynmurat@gmail.com>
2021-08-06 16:28:28 +0600

    Setting up the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import { Container } from "@material-ui/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [meanings, setMeanings] = useState([]);
+  const [word, setWord] = useState("");
 
   const dictionaryApi = async () => {
     try {
       const data = await axios.get(
         "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
       );
-      console.log(data);
+      setMeanings(data.data);
     } catch (error) {
       console.log(error);
     }
   };
 
+  console.log(meanings);
+
   useEffect(() => {
     dictionaryApi();
   }, []);
-  return <div className="App">Hello World</div>;
+
+  return (
+    <div
+      className="App"
+      style={{ height: "100vh", backgroundColor: "#282c34", color: "white" }}
+    >
+      <Container
+        maxWidth="md"
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        Word
+      </Container>
+    </div>
+  );
 }
 
 export default App;
